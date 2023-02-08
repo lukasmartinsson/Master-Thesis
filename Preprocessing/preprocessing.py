cea2f6f7f3c4f3edd8e52e708d97f6853f044c07
karl lenberg <karl.lenberg@gmail.com>
2023-02-08 15:50:10 +0100

    preprocessing

--- Preprocessing/preprocessing.py
+++ Preprocessing/preprocessing.py
@@ -3,47 +3,63 @@ from sklearn.preprocessing import RobustScaler
 from sklearn.model_selection import train_test_split
 import torch
 
-def Preprocessing(df: pd.DataFrame) -> pd.DataFrame:    
-    
-    # Drop irrelevant values
-    #df = df.drop(columns=['symbol', 'timestamp'])
+import matplotlib.pyplot as plt
 
+def Preprocessing(df: pd.DataFrame, lag:int = 1, dif_all:bool = True) -> pd.DataFrame:    
+    
     # Check for NaN values
     if df.isnull().values.any():
         print("Error in dataframe, missing value")
 
+
+    col_names = list(df.columns)
+    df = df.values
+    
+    #Difference data
+    if dif_all:
+        diff = []
+        for i in range(len(df)):
+            value = df[i] - df[i - 1]
+            diff.append(value)
+        df = diff
+    
+    df = RobustScaler().fit_transform(df)
+    df = pd.DataFrame(df,columns=col_names).drop(0)
+
+
     # Add the difference between values (will be used as label)
-    df['results'] = [df.iloc[i]['vwap'] - df.iloc[i-1]['vwap'] for i in range(len(df))]
+    res_df = [df.iloc[i]['open'] - df.iloc[i+lag]['open'] for i in range(len(df)-lag)]
+    
     
     # Resets the index and removes the first value that will have the wrong results
-    df = df[1:].reset_index()
+    df = df[:-lag].reset_index()
+    print(df.head(5))
 
-    features = df[['open', 'high', 'low', 'close', 'volume', 'trade_count','vwap']]
-    labels = df['results']
-    
+    features = df[['open', 'high', 'low', 'close', 'volume', 'trade_count','vwap']].values
+    #labels = df['results'].values
+    labels = pd.DataFrame(res_df, columns=['results']).values
+
+    print(labels)
+
+    diff_one_zero = (sum(y > 0 for y in labels)-sum(y < 0 for y in labels))/len(labels)
+    print('The diff of one and zero is: '+'{:.2%}'.format(diff_one_zero[0]))
+
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=0, shuffle = False)
     
 
-    # Apply RobustScaler to the training set
-    scaler = RobustScaler().fit(X_train)
-
-    # Apply the scaling to the sets
-    X_train_scaled = scaler.transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-
     # Convert the scaled data into tensors
-    train_X = torch.tensor(X_train_scaled).float()
-    train_X = torch.reshape(train_X,   (train_X.shape[0], 1, train_X.shape[1]))
+    X_train = torch.tensor(X_train).float()
+    X_train = torch.reshape(X_train,   (X_train.shape[0], 1, X_train.shape[1]))
     
-    test_X = torch.tensor(X_test_scaled).float()
-    test_X = torch.reshape(test_X,  (test_X.shape[0], 1, test_X.shape[1])) 
+    X_test = torch.tensor(X_test).float()
+    X_test = torch.reshape(X_test,  (X_test.shape[0], 1, X_test.shape[1])) 
 
-    train_Y = torch.tensor(y_train.values).float()
-    train_Y = train_Y.view(-1, 1)
+    y_train = torch.tensor(y_train).float()
+    y_train = y_train.view(-1, 1)
 
-    test_Y = torch.tensor(y_test.values).float()
-    test_Y = test_Y.view(-1, 1)
+    y_test = torch.tensor(y_test).float()
+    y_test = y_test.view(-1, 1)
 
     return X_train, X_test, y_train, y_test
 
