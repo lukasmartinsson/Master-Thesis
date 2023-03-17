4bec8f4c72dea994feb097c4d8462d8ca75a0374
lukasmartinsson <lukas.martinsson@gmail.com>
2023-03-17 14:29:47 +0100

    Refractorng and added optimizer

--- Preprocessing/preprocessing.py
+++ Preprocessing/preprocessing.py
@@ -2,29 +2,28 @@ import pandas as pd
 from sklearn.preprocessing import RobustScaler
 from sklearn.model_selection import train_test_split
 import torch
-import torch.utils.data as data_utils
-import numpy as np
 
 def preprocessing(df: pd.DataFrame, lag:int = 1, sequence_length:int = 128, dif_all:bool = True, 
-                  train_size:int=0.9, TSAI:bool = False, CLF:bool = False, index:str = None, data:str = "alpacca", buckets:int=1) -> tuple:
+                  train_size:int=0.9, TSAI:bool = False, CLF:bool = False, index:str = None, 
+                  data:str = "alpacca", buckets:int=1, print_info:bool=False) -> tuple:
 
     #Check if the input data comes from alpacca or twelve, and if the spy index should be added as features
-    if data == "alpacca":
+    if data == "alpacca": 
         if index is not None: #min, hour, day depending on which data
             df_index = pd.read_csv('Data\index\SPY_'+index).add_suffix('_new').rename(columns={'timestamp_new':'timestamp'})
             df = pd.merge(df,df_index, on = 'timestamp', how='left').fillna(method='ffill').drop(['symbol_new'], axis=1)
         time_data = pd.to_datetime(df['timestamp'])
         df = df.drop(['symbol', 'timestamp'], axis=1) # Drop symbol and timestamp from df
-
-    elif data == "twelve":
+    
+    elif data == "twelve": 
         if index is not None: #min, hour, day depending on which data
             df_index = pd.read_csv('Data/twelve_data/SPY_'+index).add_suffix('_new').rename(columns={'datetime_new':'datetime'})
             df = pd.merge(df,df_index, on = 'datetime', how='left').fillna(method='ffill')
         time_data = pd.to_datetime(df['datetime'])
+
         df = df.drop(['datetime'], axis=1)
 
     # Extract week and day
-    
     weekday = time_data.dt.weekday.values[1:]
     hour = time_data.dt.hour.values[1:]
 
@@ -36,19 +35,16 @@ def preprocessing(df: pd.DataFrame, lag:int = 1, sequence_length:int = 128, dif_
     change = (df['close'] - df['close'].shift(lag)).dropna().reset_index(drop=True)
 
     # Calculates the difference in columns if set to true
-    df = df.diff().dropna() if dif_all else df[1:] #Might be more efficient to use iloc so we dont have to loop through everything?
+    df = df.diff().dropna() if dif_all else df[1:]
 
     #Assign week and day after, since they are features rather than timeseries, and hence shouldnt' be differenced
-    df = df.assign(change = change,
-              weekday = weekday,
-              hour = hour )
+    df = df.assign(change = change, weekday = weekday, hour = hour )
 
     #Shift function goes backwards, we wont have the "real" results for the last "lag" instances
     df = df[:-lag]
 
     #Need to be before split to ensure that buckets have the same range
     if CLF: 
-
         # Create a new column with absolute values
         df['abs_column'] = df['change'].abs()
 
@@ -60,39 +56,65 @@ def preprocessing(df: pd.DataFrame, lag:int = 1, sequence_length:int = 128, dif_
 
         # Fill 
         df['positive_bucket'] = df['positive_bucket'].fillna(-df['negative_bucket'])+buckets-1
-        print(df.groupby('positive_bucket')['change'].agg(['min', 'max']))
+
+        # Used for df_info
+        bucket_stats = df.groupby('positive_bucket')['change'].agg(['min', 'max'])
         
         #Labels need to be 0-->2n-1
         df['change'] = df['positive_bucket']
         df = df.drop(['abs_column','negative_bucket','positive_bucket'], axis=1)
 
-        # print the resulting dataframe
-        print('Split full dataset')
-        print(df['change'].value_counts())
-
+        
+    
     # Split the dataset into test and train data --> 
     df_train, df_test = df[:int(len(df)*train_size)], df[int(len(df)*train_size)+lag:]
 
     # Scales the data using robustscaler
     scaler = RobustScaler().fit(df_train)
-    df_train_scaled = pd.DataFrame(scaler.transform(df_train), index = df_train.index, columns = df_train.columns)
-    df_test_scaled = pd.DataFrame(scaler.transform(df_test), index = df_test.index, columns = df_test.columns)
+    df_scaled = pd.DataFrame(scaler.transform(df), index = df.index, columns = df.columns)
+
+    #df_train_scaled = pd.DataFrame(scaler.transform(df_train), index = df_train.index, columns = df_train.columns)
+    #df_test_scaled = pd.DataFrame(scaler.transform(df_test), index = df_test.index, columns = df_test.columns)
 
     if CLF:
-        df_train_scaled['change'] = df_train['change']
-        df_test_scaled['change'] = df_test['change']
-        print(df_train["change"].value_counts())
-        print(df_test["change"].value_counts())
+        df_scaled['change'] = df['change']
+
+        #df_train_scaled['change'] = df_train['change']
+        #df_test_scaled['change'] = df_test['change']
+
+        # Create a new dataframe for info
+        df_info = pd.DataFrame({'Label': df['change'].value_counts().sort_index(ascending=True).index, 
+                                'Count': df['change'].value_counts().sort_index(ascending=True).values, 
+                                'Train count': df_train["change"].value_counts().sort_index(ascending=True).values,
+                                'Test count': df_test["change"].value_counts().sort_index(ascending=True).values,  
+                                'Bucket min': bucket_stats['min'].values, 
+                                'Bucket max': bucket_stats['max'].values})
+
+        if print_info:
+            df_info.loc[len(df_info)] = ['Total', df_info['Count'].sum(), 
+                                        df_info['Train count'].sum(), 
+                                        df_info['Test count'].sum(), 
+                                        df_info['Bucket min'].min(), 
+                                        df_info['Bucket max'].max()]
+            
+            print(df_info)
 
     if TSAI:
-        train_sequence = create_sequences_2(df_train_scaled, 'change', sequence_length)
-        test_sequence = create_sequences_2(df_test_scaled, 'change', sequence_length)
+        df_sequence = create_sequences_2(df_scaled, 'change', sequence_length)
+        #train_sequence = create_sequences_2(df_train_scaled, 'change', sequence_length)
+        #test_sequence = create_sequences_2(df_test_scaled, 'change', sequence_length)
     
     else:
-        train_sequence = create_sequences(df_train_scaled, 'change', sequence_length)
-        test_sequence = create_sequences(df_test_scaled, 'change', sequence_length)
+        df_sequence = create_sequences(df_scaled, 'change', sequence_length)
+        #train_sequence = create_sequences(df_train_scaled, 'change', sequence_length)
+        #test_sequence = create_sequences(df_test_scaled, 'change', sequence_length)
 
-    return train_sequence, test_sequence, scaler
+    # Split the dataset into test and train data --> 
+    X_train_sequence, X_test_sequence, Y_train_sequence, Y_test_sequence = train_test_split(df_sequence[0], df_sequence[1], test_size=1-train_size, random_state=42, shuffle = False)
+
+    #train_sequence, test_sequence = df_sequence[[:int(len(df[0])*train_size)][:int(len(df[0])*train_size)]
+
+    return (X_train_sequence, Y_train_sequence), (X_test_sequence, Y_test_sequence), scaler
 
 def create_sequences(df: pd.DataFrame, prediction:str, sequence_length:int):
 
@@ -120,3 +142,27 @@ def create_sequences_2(df: pd.DataFrame, prediction: str, sequence_length: int):
     labels = torch.tensor(df.iloc[sequence_length-1:][prediction].values, dtype=torch.float)
 
     return sequences, labels
+
+def df_buckets(df: pd.DataFrame, buckets:int=1):
+        # Create a new column with absolute values
+        df['abs_column'] = df['change'].abs()
+
+        # Divide the positive values into n buckets with custom labels
+        df.loc[df['change'] >= 0, 'positive_bucket'] = pd.qcut(df.loc[df['change'] >= 0, 'abs_column'], q=buckets, labels=False)+1
+
+        # Divide the negative values into n buckets with custom labels
+        df.loc[df['change'] < 0, 'negative_bucket'] = pd.qcut(df.loc[df['change'] < 0, 'abs_column'], q=buckets, labels=False)
+
+        # Fill 
+        df['positive_bucket'] = df['positive_bucket'].fillna(-df['negative_bucket'])+buckets-1
+        print(df.groupby('positive_bucket')['change'].agg(['min', 'max']))
+        
+        #Labels need to be 0-->2n-1
+        df['change'] = df['positive_bucket']
+        df = df.drop(['abs_column','negative_bucket','positive_bucket'], axis=1)
+
+        # print the resulting dataframe
+        print('Split full dataset')
+        print(df['change'].value_counts())
+
+        return df
